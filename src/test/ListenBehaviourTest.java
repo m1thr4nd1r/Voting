56b6f8682c76c7d5764f481f9d81b9b3efd2962f
Victor dos Santos <vitocr_santos@hotmail.com>
2014-03-09 22:19:23 -0300

    Modified the test file to reflect changes made in the agents source

--- src/test/ListenBehaviourTest.java
+++ src/test/ListenBehaviourTest.java
@@ -4,33 +4,33 @@ import junit.framework.TestCase;
 
 import org.junit.Test;
 
-import agents.CreatorAgent;
+import agents.VoterAgent;
 import behaviour.ListenBehaviour;
 
 public class ListenBehaviourTest extends TestCase{
 
 	@Test
 	public void testFind() {
-		ListenBehaviour p = new ListenBehaviour();
-		CreatorAgent c = new CreatorAgent();
+		ListenBehaviour l = new ListenBehaviour();
+		VoterAgent v = new VoterAgent();
 		String[] op = {"a","b","c","d","e"};
 		
-		c.setOptions(op);
-		p.setAgent(c);
+		v.setOptions(op);
+		l.setAgent(v);
 		
-		assertEquals(0, p.find("a"));
-		assertEquals(2, p.find("c"));
-		assertEquals(4, p.find("e"));
-		assertEquals(1, p.find("b"));
-		assertEquals(3, p.find("d"));
+		assertEquals(0, l.find("a"));
+		assertEquals(2, l.find("c"));
+		assertEquals(4, l.find("e"));
+		assertEquals(1, l.find("b"));
+		assertEquals(3, l.find("d"));
 		
-		assertEquals(4, p.find("e"));
-		assertEquals(2, p.find("c"));
-		assertEquals(0, p.find("a"));
+		assertEquals(4, l.find("e"));
+		assertEquals(2, l.find("c"));
+		assertEquals(0, l.find("a"));
 		
-		assertEquals(3, p.find("e"));
-		assertEquals(0, p.find("d"));
-		assertEquals(0, p.find("d"));
+		assertEquals(3, l.find("e"));
+		assertEquals(0, l.find("d"));
+		assertEquals(0, l.find("d"));
 	}
 
 }
