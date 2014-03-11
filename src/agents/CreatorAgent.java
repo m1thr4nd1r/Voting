727a5a4af6a397c5be432226911c73903f2245e5
Victor dos Santos <vitocr_santos@hotmail.com>
2014-03-10 22:56:10 -0300

    Added voting type to the agent parameters and a winner information

--- src/agents/CreatorAgent.java
+++ src/agents/CreatorAgent.java
@@ -14,9 +14,10 @@ import jade.wrapper.StaleProxyException;
 public class CreatorAgent extends Agent 
 {	
 	private int optionQnt = 0;
-	private String[] options = null;
 	private int agentQnt = 0;
+	private String[] options = null;
 	private int[] votes;
+	private String type = null;
 	
 	protected void setup()
 	{
@@ -24,9 +25,13 @@ public class CreatorAgent extends Agent
 		if (args != null && args.length > 0)
 		{
 //			Numero de Açoes, Acão1, Acão2,..., Numero de Agentes
+//			Tipo de Votação,Numero de Açoes, Acão1, Acão2,..., Numero de Agentes
+			
+//			Extraindo tipo de votação
+			type = (String) args[0];
 			
 //			Extraindo numero de opções disponiveis
-			optionQnt = Integer.parseInt((String) args[0]);
+			optionQnt = Integer.parseInt((String) args[1]);
 //			System.out.println("Quantidade de opçoes: " + optionQnt);
 			
 			options = new String[optionQnt];
@@ -34,10 +39,10 @@ public class CreatorAgent extends Agent
 			
 //			Extraindo opções disponiveis
 			for (int i = 0; i < optionQnt; i++)
-				options[i] = (String) args[i+1];
+				options[i] = (String) args[i+2];
 			
 //			Extraindo o numero de agentes envolvidos na votação
-			agentQnt = Integer.parseInt((String) args[optionQnt+1]);
+			agentQnt = Integer.parseInt((String) args[optionQnt+2]);
 //			System.out.println("Quantidade de agentes: " + agentQnt);
 	
 //			Criando variaveis necessarias a criação de um novo agente (em um novo container)
@@ -61,7 +66,53 @@ public class CreatorAgent extends Agent
 			printOptions();
 			
 			this.addBehaviour(new PoolingBehaviour(this, agentQnt));
-			this.addBehaviour(new StartBehaviour(this, "Simple", agentQnt));
+			this.addBehaviour(new StartBehaviour(this, type, agentQnt));
+		}
+	}
+	
+	protected void takeDown() 
+	{
+		if (type.equals("Simple"))
+		{
+			int max = 0;
+			int winner = -1;
+			
+			for (int i = 0; i < optionQnt; i++)
+			{
+				if (votes[i] > max)
+				{
+					max = votes[i];
+					winner = i;
+				}
+			}
+			
+			System.out.println("O ganhador da eleição é '" + options[winner] + "' com " + max + " votos");
+		}
+		else if (type.equals("Borda"))
+		{
+			for (int i = 0; i < optionQnt; i++)
+				for (int j = 0; j < optionQnt; j++)
+					if (votes[i] > votes[j])
+					{
+						int tempI = votes[i];
+						String tempS = options[i];
+						
+						votes[i] = votes[j];
+						options[i] = options[j];
+						
+						votes[j] = tempI;
+						options[j] = tempS;
+					}
+			
+			System.out.print("A eleição determinou a seguinte prioridade: ");
+						
+			for (int i = 0; i < optionQnt; i++)
+				System.out.print(options[i] + " ");
+			
+			System.out.print("\n   Cada opção teve a seguinte pontuação   : ");
+			
+			for (int i = 0; i < optionQnt; i++)
+				System.out.print(votes[i] + " ");
 		}
 	}
 	
@@ -75,9 +126,18 @@ public class CreatorAgent extends Agent
 		return votes[index];
 	}
 	
+	public String getType() {
+		return type;
+	}
+
 	public void increment(int index)
 	{
 		votes[index]++;
+	}
+	
+	public void increment(int index, int amount)
+	{
+		votes[index]+= amount;
 	}
 	
 	public void printOptions()
